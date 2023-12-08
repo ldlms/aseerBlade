1bc78c9a381b141d2c9dbc7ef01de12d96e0253e
Leo <leodelmas31@gmail.com>
2023-12-08 16:45:16 +0100

    first

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,18 @@
+<nav class="bg-blue-200 py-4">
+  <div class="container mx-auto flex justify-between items-center">
+    <!-- Logo ou titre à gauche -->
+    <div class="flex items-center">
+      <div class="text-white text-lg font-bold mr-4">Mon Site</div>
+      
+      <!-- Liens de navigation -->
+      <ul class="flex space-x-4">
+        <li><a href="#" class="text-white hover:text-gray-300">Accompagnement scolaire</a></li>
+        <li><a href="#" class="text-white hover:text-gray-300">Projets</a></li>
+        <li><a href="#" class="text-white hover:text-gray-300">Univers-cité d'été</a></li>
+        <li><a href="#" class="text-white hover:text-gray-300">Devenir bénévole</a></li>
+        <li><a href="#" class="text-white hover:text-gray-300">Articles</a></li>
+        <li><a href="#" class="text-white hover:text-gray-300">Contact</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
