5696f4bf12845cdcc41fe6d354d3fa9cad5fb300
Leo <leodelmas31@gmail.com>
2023-12-08 17:40:09 +0100

    tailwind

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,18 +1,37 @@
-<nav class="bg-blue-200 py-4">
-  <div class="container mx-auto flex justify-between items-center">
-    <!-- Logo ou titre à gauche -->
-    <div class="flex items-center">
-      <div class="text-white text-lg font-bold mr-4">Mon Site</div>
-      
-      <!-- Liens de navigation -->
-      <ul class="flex space-x-4">
-        <li><a href="#" class="text-white hover:text-gray-300">Accompagnement scolaire</a></li>
-        <li><a href="#" class="text-white hover:text-gray-300">Projets</a></li>
-        <li><a href="#" class="text-white hover:text-gray-300">Univers-cité d'été</a></li>
-        <li><a href="#" class="text-white hover:text-gray-300">Devenir bénévole</a></li>
-        <li><a href="#" class="text-white hover:text-gray-300">Articles</a></li>
-        <li><a href="#" class="text-white hover:text-gray-300">Contact</a></li>
-      </ul>
-    </div>
-  </div>
+<!-- mobile -->
+<div class=" bg-blue-800 lg:hidden">
+<header class="text-center py-4">
+    <img src="votre-logo.png" alt="Logo" class="mx-auto h-12">
+    <h1 class="text-lg font-semibold">Nom du Site</h1>
+</header>
+
+<!-- Menu burger pour mobile -->
+<div class="flex justify-between items-center lg:hidden py-4">
+<button class="block text-white focus:outline-none lg:hidden">
+            <!-- Icône de menu burger -->
+            <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
+                <path
+                    d="M4 6h16M4 12h16M4 18h16"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                />
+            </svg>
+        </button>
+</div>
+</div>
+
+<!-- desktop -->
+<div class=" bg-blue-800 hidden lg:block">
+<header class="text-center py-4">
+    <img src="votre-logo.png" alt="Logo" class="mx-auto h-12">
+    <h1 class="text-lg font-semibold">A.S.E.E.R</h1>
+</header>
+<nav class="flex justify-center items-center py-4 hidden lg:flex">
+    <a href="#" class="text-white px-4">Menu 1</a>
+    <a href="#" class="text-white px-4">Menu 2</a>
+    <a href="#" class="text-white px-4">Menu 3</a>
+    <!-- ... Ajoutez d'autres éléments de menu ... -->
 </nav>
+</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,14 @@ module.exports = {
     "./resources/**/*.vue",
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '640px',  // Petit écran (mobile)
+        'md': '768px',  // Écran moyen (tablettes)
+        'lg': '1024px', // Grand écran (ordinateur de bureau)
+        'xl': '1280px', // Très grand écran (grandes résolutions)
+      },
+    },
   },
   plugins: [],
 }
